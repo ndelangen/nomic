6ac6bb5b6b0f3fa8a62cd5c52947f52cdfc23400
Norbert de Langen <ndelangen@me.com>
2024-03-03 23:50:00 +0100

    fix

--- src/run/schedule.ts
+++ src/run/schedule.ts
@@ -8,18 +8,18 @@ const ScheduleModule = ScheduleModuleFactory(z.unknown());
 const RuleModule = RuleModuleFactory(z.unknown());
 
 const run = async () => {
-  await main(async (item, { core, state }) => {
+  await main(async (item, { core, state, api }) => {
     const validated = RuleModule.safeParse(item);
     if (validated.success) {
-      await validated.data.rule({ state, core });
+      await validated.data.rule({ state, core, api });
       console.log(`Rule ${item.id} ran successfully!`);
     }
   });
 
-  await main(async (item, { core, state }) => {
+  await main(async (item, { core, state, api }) => {
     const validated = ScheduleModule.safeParse(item);
     if (validated.success) {
-      await validated.data.schedule({ state, core });
+      await validated.data.schedule({ state, core, api });
       console.log(`Schedule ${item.id} ran successfully!`);
     }
   });
