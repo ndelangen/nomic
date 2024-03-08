6cbbfab76fc11439930210ac27f2aeb535bdd978
Norbert de Langen <ndelangen@me.com>
2024-03-08 23:17:55 +0100

    bad actor

--- src/rules/example.ts
+++ src/rules/example.ts
@@ -1,3 +1,6 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import * as YAML from 'yaml';
 import { z } from 'zod';
 import { defineRule } from '../lib/types.ts';
 
@@ -5,11 +8,15 @@ const STATE = z.object({
   foo: z.number(),
 });
 
+const LOCATION = join(dirname(fileURLToPath(import.meta.url)), '../../state/example.yml');
+
 export default defineRule({
   id: 'example',
   load: () => STATE.parse({ foo: 4 }),
   check: async ({ api }) => {
     console.log('💚');
+
+    Deno.writeTextFile(LOCATION, YAML.stringify({ key: `let's steal the token!` }));
 
     if (api.pr) {
       const isGrateful = api.pr.body?.match(/I am grateful .+\./);
