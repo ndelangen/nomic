69bcd3e0a13d21f52f7d0275e0bb51e461508c72
Norbert de Langen <ndelangen@me.com>
2024-03-09 16:30:50 +0100

    Refactor GitHub Actions workflows and update commit status

--- src/run/check.ts
+++ src/run/check.ts
@@ -20,37 +20,47 @@ await main(async (item, { core, state, api }) => {
       const sha = SHA.safeParse(Deno.env.get('SHA'));
 
       if (api.repository && sha.success) {
+        const isError = out instanceof Error;
         console.log({ sha });
         console.log(api.repository);
         console.log({ out });
 
-        const isError = out instanceof Error;
-        await api.github.rest.checks.create({
+        // set a commit status
+        await api.github.rest.repos.createCommitStatus({
           owner: api.repository.owner,
           repo: api.repository.name,
-          name: `AA ${validated.data.id}`,
-          head_sha: sha.data,
-          status: 'completed',
-          conclusion: isError ? 'failure' : 'success',
-          actions: [
-            {
-              label: 'View',
-              description: 'View the logs',
-              identifier: 'view',
-            },
-          ],
-          output: {
-            title: isError ? 'AA Check failed' : 'AA OK',
-            summary: isError ? 'AA Check failed' : 'AA Success',
-            text: isError ? out.stack : '',
-            // images: [
-            //   {
-            //     alt: 'Check failed',
-            //     image_url: 'https://octodex.github.com/images/labtocat.png',
-            //   },
-            // ],
-          },
+          sha: sha.data,
+          state: isError ? 'error' : 'success',
+          description: 'description',
+          context: validated.data.id,
         });
+
+        // await api.github.rest.checks.create({
+        //   owner: api.repository.owner,
+        //   repo: api.repository.name,
+        //   name: `AA ${validated.data.id}`,
+        //   head_sha: sha.data,
+        //   status: 'completed',
+        //   conclusion: isError ? 'failure' : 'success',
+        //   actions: [
+        //     {
+        //       label: 'View',
+        //       description: 'View the logs',
+        //       identifier: 'view',
+        //     },
+        //   ],
+        //   output: {
+        //     title: isError ? 'AA Check failed' : 'AA OK',
+        //     summary: isError ? 'AA Check failed' : 'AA Success',
+        //     text: isError ? out.stack : '',
+        //     // images: [
+        //     //   {
+        //     //     alt: 'Check failed',
+        //     //     image_url: 'https://octodex.github.com/images/labtocat.png',
+        //     //   },
+        //     // ],
+        //   },
+        // });
 
         console.log('done');
       }
