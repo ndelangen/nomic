b23d84ce108227addd06177d1f23d5a43c909595
Norbert de Langen <ndelangen@me.com>
2024-03-09 22:47:20 +0100

    Refactor getPrInfo function and add logic to retrieve pull request number from SHA

--- src/core/api.ts
+++ src/core/api.ts
@@ -2,17 +2,21 @@ import { Octokit } from 'octokit';
 import { z } from 'zod';
 
 const ARGS = z.tuple([z.string(), z.string()]);
+const SHA = z.string();
 
 export const Repository = z.object({
   owner: z.string(),
   name: z.string(),
 });
 
-export async function getPrInfo(octokit: Octokit, repository: z.infer<typeof Repository>) {
+export async function getPrInfo(
+  octokit: Octokit,
+  repository: z.infer<typeof Repository>,
+  pull_number: number,
+) {
   const eventName = Deno.env.get('GITHUB_EVENT_NAME');
 
   if (eventName === 'pull_request') {
-    const pull_number = parseInt(Deno.env.get('PR_NUMBER')!, 10);
     const [pr, reviews] = await Promise.all([
       octokit.rest.pulls.get({
         owner: repository.owner,
@@ -36,10 +40,29 @@ const createOctokit = () => new Octokit({ auth: Deno.env.get('GITHUB_TOKEN') });
 
 export const defineAPI = async () => {
   const octokit = createOctokit();
-  const result = ARGS.safeParse(Deno.env.get('GITHUB_REPOSITORY')?.split('/'));
+  const repo = ARGS.safeParse(Deno.env.get('GITHUB_REPOSITORY')?.split('/'));
+  const sha = SHA.safeParse(Deno.env.get('SHA'));
+  let pull_number = parseInt(Deno.env.get('PR_NUMBER')!, 10);
 
-  const repository = result.success ? { owner: result.data[0], name: result.data[1] } : undefined;
-  const pr = result.success ? await getPrInfo(octokit, Repository.parse(repository)) : undefined;
+  const repository = repo.success ? { owner: repo.data[0], name: repo.data[1] } : undefined;
+
+  if (sha.success && repository && !pull_number) {
+    const prs = await octokit.rest.pulls.list({
+      owner: repository.owner,
+      repo: repository.name,
+      state: 'open',
+      base: 'main',
+      per_page: 10,
+    });
+
+    const pr = prs.data.find((pr) => pr.head.sha === sha.data);
+
+    if (pr) {
+      pull_number = pr.number;
+    }
+  }
+
+  const pr = repo.success ? await getPrInfo(octokit, Repository.parse(repository), pull_number) : undefined;
 
   return { pr, github: octokit, repository };
 };
