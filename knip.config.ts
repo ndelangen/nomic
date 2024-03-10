2bb51cf208d8635ae0062cc0efd7dd9589c4cc35
Norbert de Langen <ndelangen@me.com>
2024-03-10 14:05:59 +0100

    Add ignore pattern for tests

--- knip.config.ts
+++ knip.config.ts
@@ -3,4 +3,5 @@ import type { KnipConfig } from 'knip';
 export default {
   entry: ['./run/*.ts', './rules/*.ts', './core/rule.ts'],
   project: ['./**/*.ts'],
+  ignore: ['**/tests/**'],
 } satisfies KnipConfig;
