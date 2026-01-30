938731a29cfd37a0a32ee4fdf9a9fc3f27360015
Norbert de Langen <ndelangen@me.com>
2026-01-30 19:32:20 +0100

    improve knip config

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,8 @@
 import type { KnipConfig } from 'knip';
 
 export default {
-  entry: ['./run/*.ts', './rules/*.ts', './core/rule.ts'],
+  entry: ['./run/*.ts', './rules/*.ts'],
   project: ['./**/*.ts'],
   ignore: ['**/tests/**'],
+  ignoreDependencies: ['bun-types'],
 } satisfies KnipConfig;
